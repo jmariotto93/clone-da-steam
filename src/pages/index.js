36477079289ad3df893d1a1dc4a2bdd435235b9d
José Mariotto <josemarioto@gmail.com>
2023-07-15 19:34:36 -0300

    begin

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <Head>
         <title>DevSteam: A sua loja online de games</title>
-        <meta name="description" content="DevSteam: A sua loja online de games" />
+        <meta name="description" content="Steam: A sua loja online de games" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>

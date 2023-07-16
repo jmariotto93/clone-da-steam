07a422a2feb08beb52d0d4b2b2199dbeea4d4cd3
José Mariotto <josemarioto@gmail.com>
2023-07-15 21:29:24 -0300

    create navbar

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './logo.module.css'
+
+export default function Logo() {
+    return(
+        <figure className={styles.logo}>
+           <Image src='/logo.svg' width={60} height={60}/>
+           <h1>Steam</h1>
+        </figure>
+    )
+}

42c9d4cef212a90be9d6eb859d0ee870ded87dc0
José Mariotto <josemarioto@gmail.com>
2023-07-24 21:02:28 -0300

    Create Cart Menu

---
+++ src/components/forms/cartButton/cartButton.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { BsCart4 } from 'react-icons/bs'
+
+import styles from './cartButton.module.css'
+import CartMenu from '@/components/cartMenu/cartmenu'
+
+export default function CartButton () {
+    const [open, setOpen] = useState(false)
+
+    return (
+        <div>
+           <BsCart4 
+           className={styles.icon} 
+           size={40} 
+
+           onClick={() => {setOpen(!open)}}
+           />
+           {open && <CartMenu />}
+        </div>
+    )
+}
